17cd369572a199fe9aa2dfce487dc5e95c08ec1f
fincautopia <juanfelixcastrosoto@gmail.com>
2024-11-23 17:29:03 -0600

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Smooth scrolling for anchor links (optional if you want smooth scroll in sections)
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
