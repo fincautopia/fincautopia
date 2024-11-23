c39e45fc013175801277053a6490be406e4f9cd7
fincautopia <juanfelixcastrosoto@gmail.com>
2024-11-23 17:02:52 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scrolling for anchor links (optional if you want smooth scroll in sections)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
